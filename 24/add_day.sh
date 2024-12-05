fe596f4ba74314e49bc61b49d970f6a5fb0c7f65
simon <sbmp2000@gmail.com>
2024-12-04 22:01:19 -0800

    idea for lazy automation of new days

---
+++ 24/add_day.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#simon parker
+#goal: run this to add a folder for a new day, meaning i need to find the biggest day in the directory and add 1 to it,
+#        then add a blank test.txt, input.txt, 1.py, 2.py
